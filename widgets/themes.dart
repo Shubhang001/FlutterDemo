14c9a871ccf98afb3376a1f794f49c00caab07c1
Shubhang <shubhangshukla001@gmail.com>
2023-06-04 10:20:55 +0530

    dark buish and cream color committed

--- widgets/themes.dart
+++ widgets/themes.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme{
   static ThemeData lightTheme(BuildContext context)=> ThemeData(
         primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
+        fontFamily: GoogleFonts.poppins().fontFamily,
         appBarTheme: const AppBarTheme(
           color: Colors.white,
           iconTheme: IconThemeData(color: Colors.black),
@@ -15,4 +15,7 @@ class MyTheme{
         brightness: Brightness.dark,
         primarySwatch: Colors.brown,
       );
-}
+
+  static Color creamColor = const Color(0xfff5f5f5);
+  static Color darkBluishColor = const Color(0xff403b58);
+     }
